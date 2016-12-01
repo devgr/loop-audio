c243dd87eb2c63a9f31f05aa3db17052304242d9
ctking42 <ctking42@gmail.com>
2016-11-30 20:05:07 -0600

    Updated UI with color scheme and pan knobs

--- js/app.js
+++ js/app.js
@@ -1,37 +1,29 @@
-function togglePic(x){
-    if(document.getElementById(x).value === 'Off'){
-        document.getElementById(x).src = "padRed.png"
-        document.getElementById(x).value = 'On'
+function handleClick(x){
+
+	if(document.getElementById(x).value === 'Ready'){
+		document.getElementById(x).src = "padRed.png";
+		document.getElementById(x).value = 'Rec';
+        //Record loop
+        libLoop.CreateTrack(x);
+	}
+	else if(document.getElementById(x).value === 'Off' || document.getElementById(x).value ==='Rec'){
+        document.getElementById(x).src = "padOrange.png";
+        document.getElementById(x).value = 'On';
+        //Play loop
     }
     else{
-        document.getElementById(x).src = "padOrange.png"
-        document.getElementById(x).value = 'Off'
+        document.getElementById(x).src = "padYellow.png";
+        document.getElementById(x).value = 'Off';
+        //pause loop
     }
 }
 
 function eraseLoop(x){
-	document.getElementById(x).src = "padGreen.png"
-	document.getElementById(x).value = 'Off'
+	document.getElementById(x).src = "padGreen.png";
+	document.getElementById(x).value = 'Ready';
+    //Erase loop
 }
 
+//document.getElementById("looper").onkeydown =
 
 
-/*new Vue({
-	el: '#looper',
-	data: {
-	    counter: 0
-    },
-	methods: {
-		toggle: function (x) {
-			if(x.className == "Off"){
-				x.src = "padOn.png";
-				x.className = "On";
-			}
-			else{
-				x.src = "padOff.png";
-				x.className = "Off";
-			}
-			return false;
-		}
-	}
-})();*/
